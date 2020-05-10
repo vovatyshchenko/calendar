f517ca405eccd501da332e40d3fd6f5d7aad56be
Max Shcherbinin <maksimserbinin9119@gmail.com>
2020-05-11 01:48:53 +0300

    validation

--- resources/js/components/layouts/menu/blocks/modals/Activity.vue
+++ resources/js/components/layouts/menu/blocks/modals/Activity.vue
@@ -153,25 +153,9 @@
 </template>
 
 <script>
-    import { validationMixin } from 'vuelidate'
-    import { required, email } from 'vuelidate/lib/validators'
+    import validation from '../../../../../mixin/validation'
     export default {
-        mixins: [validationMixin],
-        validations: {
-            name: { required},
-            guests: {required},
-            email: { required, email },
-            select: { required },
-            location: { required },
-            description: { required },
-            dateStart:{required},
-            dateStart:{required},
-            checkbox: {
-                checked (val) {
-                    return val
-                },
-            },
-        },
+        mixins: [validation],
         data: () => ({
             name:null,
             menu1: false,
@@ -187,80 +171,6 @@
             openDataEnd: false,
             description:null,
         }),
-        computed: {
-            computedDateFormattedMomentjs () {
-                return this.dateStart ? moment(this.dateStart).format('D MMM  YYYY') : ''
-            },
-            computedDateFormattedDatefns () {
-                return this.dateStart ? format(this.dateStart, 'D MMM   YYYY') : ''
-            },
-            computedDateFormattedMomentjsForEnd () {
-                return this.dateEnd ? moment(this.dateEnd).format('D MMM  YYYY') : ''
-            },
-            computedDateFormattedDatefnsForEnd () {
-                return this.dateEnd ? format(this.dateEnd, 'D MMM   YYYY') : ''
-            },
-            checkboxErrors () {
-                const errors = []
-                if (!this.$v.checkbox.$dirty) return errors
-                !this.$v.checkbox.checked && errors.push('You must agree to continue!')
-                return errors
-            },
-            selectErrors () {
-                const errors = []
-                if (!this.$v.select.$dirty) return errors
-                !this.$v.select.required && errors.push('Item is required')
-                return errors
-            },
-            dateStartErrors () {
-                const errors = []
-                if (!this.$v.select.$dirty) return errors
-                !this.$v.select.required && errors.push('Item is required')
-                return errors
-            },
-            nameErrors () {
-                const errors = []
-                if (!this.$v.name.$dirty) return errors
-                !this.$v.name.required && errors.push(' ')
-                return errors
-            },
-            guestsErrors () {
-                const errors = []
-                if (!this.$v.guests.$dirty) return errors
-                !this.$v.guests.required && errors.push(' ')
-                return errors
-            },
-            emailErrors () {
-                const errors = []
-                if (!this.$v.email.$dirty) return errors
-                !this.$v.email.email && errors.push('Must be valid e-mail')
-                !this.$v.email.required && errors.push()
-                return errors
-            },
-            locationErrors () {
-                const errors = []
-                if (!this.$v.location.$dirty) return errors
-                !this.$v.location.required && errors.push(' ')
-                return errors
-            },
-            descriptionErrors () {
-                const errors = []
-                if (!this.$v.description.$dirty) return errors
-                !this.$v.description.required && errors.push(' ')
-                return errors
-            },
-            globalErrorMessasge(){
-                if(this.nameErrors.length>0
-                    ||this.guestsErrors.length>0
-                    ||this.locationErrors.length>0
-                    ||this.descriptionErrors.length>0
-                )
-                {
-                    return true;
-                }
-                return false;
-            }
-        },
         methods: {
             submit () {
                 this.$v.$touch()
--- resources/js/mixin/validation.js
+++ resources/js/mixin/validation.js
@@ -1,34 +1,100 @@
 
+import { validationMixin } from 'vuelidate'
+import { required, email } from 'vuelidate/lib/validators'
 export default {
+    mixins: [validationMixin],
     data(){
         return{
 
         }
     },
-    computed:{
-        holidayTextEventForDay()
-        {
-            let dateForMonth = this.$store.getters.menuDate;
-            if (this.$store.getters.holidays[dateForMonth.getMonth() + 1]) {
-                if (this.$store.getters.holidays[dateForMonth.getMonth() + 1][dateForMonth.getDate()]) {
-
-                    return  this.$store.getters.holidays[dateForMonth.getMonth() + 1][dateForMonth.getDate()].localName;
-                }
-            }
+    validations: {
+        name: { required},
+        guests: {required},
+        email: { required, email },
+        select: { required },
+        location: { required },
+        description: { required },
+        dateStart:{required},
+        dateStart:{required},
+        checkbox: {
+            checked (val) {
+                return val
+            },
         },
-        holidayTextEvent()
-        {
-            console.log(this.date);
-            let parseDate = this.date.split("-");
-
-            if(this.$store.getters.holidays[parseDate[1]])
+    },
+    computed: {
+        computedDateFormattedMomentjs () {
+            return this.dateStart ? moment(this.dateStart).format('D MMM  YYYY') : ''
+        },
+        computedDateFormattedDatefns () {
+            return this.dateStart ? format(this.dateStart, 'D MMM   YYYY') : ''
+        },
+        computedDateFormattedMomentjsForEnd () {
+            return this.dateEnd ? moment(this.dateEnd).format('D MMM  YYYY') : ''
+        },
+        computedDateFormattedDatefnsForEnd () {
+            return this.dateEnd ? format(this.dateEnd, 'D MMM   YYYY') : ''
+        },
+        checkboxErrors () {
+            const errors = []
+            if (!this.$v.checkbox.$dirty) return errors
+            !this.$v.checkbox.checked && errors.push('You must agree to continue!')
+            return errors
+        },
+        selectErrors () {
+            const errors = []
+            if (!this.$v.select.$dirty) return errors
+            !this.$v.select.required && errors.push('Item is required')
+            return errors
+        },
+        dateStartErrors () {
+            const errors = []
+            if (!this.$v.select.$dirty) return errors
+            !this.$v.select.required && errors.push('Item is required')
+            return errors
+        },
+        nameErrors () {
+            const errors = []
+            if (!this.$v.name.$dirty) return errors
+            !this.$v.name.required && errors.push(' ')
+            return errors
+        },
+        guestsErrors () {
+            const errors = []
+            if (!this.$v.guests.$dirty) return errors
+            !this.$v.guests.required && errors.push(' ')
+            return errors
+        },
+        emailErrors () {
+            const errors = []
+            if (!this.$v.email.$dirty) return errors
+            !this.$v.email.email && errors.push('Must be valid e-mail')
+            !this.$v.email.required && errors.push()
+            return errors
+        },
+        locationErrors () {
+            const errors = []
+            if (!this.$v.location.$dirty) return errors
+            !this.$v.location.required && errors.push(' ')
+            return errors
+        },
+        descriptionErrors () {
+            const errors = []
+            if (!this.$v.description.$dirty) return errors
+            !this.$v.description.required && errors.push(' ')
+            return errors
+        },
+        globalErrorMessasge(){
+            if(this.nameErrors.length>0
+                ||this.guestsErrors.length>0
+                ||this.locationErrors.length>0
+                ||this.descriptionErrors.length>0
+            )
             {
-                if(this.$store.getters.holidays[parseDate[1]][parseDate[0]])
-                {
-                    console.log(this.$store.getters.holidays[parseDate[1]][parseDate[0]].localName);
-                    return this.$store.getters.holidays[parseDate[1]][parseDate[0]].localName;
-                }
+                return true;
             }
+            return false;
         }
-    }
+    },
 }

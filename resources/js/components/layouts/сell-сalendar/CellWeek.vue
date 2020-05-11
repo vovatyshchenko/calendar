27cda0a7145e360be53879d46dc48837880dc988
Max Shcherbinin <maksimserbinin9119@gmail.com>
2020-05-11 23:53:29 +0300

    style_event_week

--- resources/js/components/layouts/сell-сalendar/CellWeek.vue
+++ resources/js/components/layouts/сell-сalendar/CellWeek.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <div class="week-event-header week-header">
-            <span>{{getDayWeek}}</span>
+            <div>
+                <span>{{getDayWeek}} </span>
+                 <span class="event">{{holidayTextEvent}}</span>
+            </div>
+
         </div>
         <div class="week-events hour-block" v-for="n in 24">
             <hour-block></hour-block>
@@ -10,8 +14,10 @@
 </template>
 
 <script>
+    import holiday from '../../../mixin/holiday'
     export default {
         props:['date'],
+        mixins: [holiday],
         data(){
             return{
                 days: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
@@ -19,10 +25,8 @@
         },
         computed: {
             getDayWeek() {
-                let currentDate = this.date[0];
-                let parseDate = currentDate.split("-");
+                let parseDate = this.date.split("-");
                 let dayNumber= new Date(parseDate[2], parseDate[1]-1, parseDate[0]).getDay();
-
                 return this.days[dayNumber]+', '+parseDate[0];
             }
         }

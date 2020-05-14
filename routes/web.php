5950b5622e2a0e0b3af28a4f8f599c4ecb0630ee
Max Shcherbinin <maksimserbinin9119@gmail.com>
2020-05-14 10:39:03 +0300

    edit seed

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,16 @@ require_once('Holidays/index.php');
 Route::get('/auth/redirect','Auth\AuthController@authorizathion')->name('login');
 Route::get('/auth/callback', 'Auth\AuthController@callback');
 
-//Route::view('/', 'index')->middleware('auth');
+Route::view('/', 'index')->middleware('auth');
 Route::post('/events', ('Api\v1\EventsController@getEvents'));
 Route::post('/create-activity', ('Api\v1\ActivityController@store'));
 Route::post('/create-task', ('Api\v1\TaskController@store'));
 Route::post('/create-birthday', ('Api\v1\BirthdayController@store'));
-Route::view('/{any}', 'index')->where('any', '.*');
+
 Route::delete('/delete-activity', ('Api\v1\ActivityController@destroy'));
 Route::delete('/delete-task', ('Api\v1\TaskController@destroy'));
 Route::delete('/delete-birthday/{id}', ('Api\v1\BirthdayController@destroy'));
+
+Route::view('/{any}', 'index')->where('any', '.*');
 //Route::middleware('auth')->post('/logout', 'Auth\LogoutController@logout');
 

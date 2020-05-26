0a007f17f6099a4607513d669bb5767d4fb53dab
Max Shcherbinin <maksimserbinin9119@gmail.com>
2020-05-26 23:03:35 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-require_once('Alexandr/api.php');
-require_once('Maxim/api.php');
-require_once('Vitaliy/api.php');
 require_once('Vladimir/api.php');
 
 
